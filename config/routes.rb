3146d4079767199cadc2279dee0ddb92fd968caf
takurofukuta <mojuraspax@icloud.com>
2021-04-08 11:46:50 +0000

    resourcesでequipmentsコントローラのパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'equipments#index'
+  resources :equipments
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
