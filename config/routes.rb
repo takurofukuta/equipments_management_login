46979a540329b9ceb448a5c73da43353aefe2d48
takurofukuta <mojuraspax@icloud.com>
2021-04-08 11:45:58 +0000

    rootをequipments#indexに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'equipments/index'
-  get 'equipments/show'
-  get 'equipments/edit'
-  root 'home#top'
+  root 'equipments#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
