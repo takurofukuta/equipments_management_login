eaee825e0cf1aaacfc9af16f631c15f40275cb19
takurofukuta <mojuraspax@icloud.com>
2021-04-08 12:29:00 +0000

    Equipments#indexにequipment.idが表示されるようにした

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,5 +1,6 @@
 class EquipmentsController < ApplicationController
   def index
+    @equipments = Equipment.all
   end
 
   def show
@@ -7,4 +8,5 @@ class EquipmentsController < ApplicationController
 
   def edit
   end
+  
 end
