e85284944fd2ab75673d88a6a974e6019fd391f3
takurofukuta <mojuraspax@icloud.com>
2021-04-08 11:45:12 +0000

    equipmentsコントローラを作成

---
+++ app/controllers/equipments_controller.rb
@@ -0,0 +1,10 @@
+class EquipmentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'equipments/index'
+  get 'equipments/show'
+  get 'equipments/edit'
   root 'home#top'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
