0ae0a9ebdced3af6def73da1d4b2a12b2db71596
takurofukuta <mojuraspax@icloud.com>
2021-04-11 14:13:29 +0000

    Lendingコントローラを作成

---
+++ db/migrate/20210411141314_create_lendings.rb
@@ -0,0 +1,11 @@
+class CreateLendings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lendings do |t|
+      t.integer :lending_status
+      t.references :user, null: false, foreign_key: true
+      t.references :equipment, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
