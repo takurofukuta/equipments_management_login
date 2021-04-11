72f3e70b94471b92bc7514092ec007aaf57d35e7
takurofukuta <mojuraspax@icloud.com>
2021-04-11 13:45:19 +0000

    equipmentモデルにバリデーションを追加

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,8 @@
 class Equipment < ApplicationRecord
   belongs_to :user, optional: true
+  validates :genre, presence: true
+  validates :lab_equipment_name, presence: true
+  validates :maker_name, presence: true
+  validates :product_name, presence: true
+  validates :purchase_year, presence: true
 end
