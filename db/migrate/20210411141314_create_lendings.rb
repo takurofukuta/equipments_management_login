40a546a56d9a173417456c5011fedcab0c03ccf3
takurofukuta <mojuraspax@icloud.com>
2021-04-11 15:08:03 +0000

    lending_statusカラムをlendings→equipmentsモデルに移動

--- db/migrate/20210411141314_create_lendings.rb
+++ db/migrate/20210411141314_create_lendings.rb
@@ -1,7 +1,6 @@
 class CreateLendings < ActiveRecord::Migration[6.1]
   def change
     create_table :lendings do |t|
-      t.integer :lending_status
       t.references :user, null: false, foreign_key: true
       t.references :equipment, null: false, foreign_key: true
 
