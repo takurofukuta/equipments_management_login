ba22f7bfe75f427a5280802fe6da920f7b510815
takurofukuta <mojuraspax@icloud.com>
2021-04-13 23:23:08 +0900

    貸出・返却機能を追加

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -1,9 +1,16 @@
 class LendingsController < ApplicationController
   def index
-    @lendings = Lending.all
+    @lendings = Equipment.where(lendings_status: 1)
   end
 
   def update
     equipment = Equipment.find(params[:id])
+    if equipment.lendings_status == 0
+      equipment.lendings_status = 1
+    elsif equipment.lendings_status == 1
+      equipment.lendings_status = 0
+    end
+    equipment.save
+    redirect_to root_path
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Equipment.destroy_all
 user = User.first
-20.times do 
-Equipment.create!(genre: "test", lab_equipment_name: "test", maker_name: "test", product_name: "test", purchase_year: 1111, asset_num: "test",
+20.times do |i|
+Equipment.create!(genre: "test", lab_equipment_name: "test#{i}", maker_name: "test", product_name: "test", purchase_year: 1111, asset_num: "test",
 price: 1111, remarks: "test", user_id: user.id )
 end
 puts "初期データの作成完了"
