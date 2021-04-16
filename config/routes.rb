460edd86baf2e694940fc6caae2119e3b99171eb
takurofukuta <mojuraspax@icloud.com>
2021-04-16 18:43:39 +0900

    貸出履歴ページのビューとルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "equipments#index"
   resources :equipments
   resources :lendings
+  get "/lendings/lendings_history", to: "lendings_history#index"
   devise_for :users, controllers: {
                        sessions: "users/sessions",
                        registrations: "users/registrations",
