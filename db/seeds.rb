fc41f35337778ceb01f0e6c9c0e5d8160a0bbdf8
takurofukuta <mojuraspax@icloud.com>
2021-04-17 18:01:44 +0900

    fakerを用いてseedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
+require "csv"
+require "faker"
+
 User.destroy_all
 Equipment.destroy_all
 user = User.create!(user_name: "takurofukuta", last_name: "福田", first_name: "拓郎", assignment_year: 2016, password: "password")
-10.times do |i|
-Equipment.create!(genre: "test", lab_equipment_name: "test#{i}", maker_name: "test", product_name: "test", purchase_year: 1111, asset_num: "test",
-price: 1111, remarks: "test", user_id: user.id )
+1000.times do |i|
+Equipment.create!(genre: Faker::Device.manufacturer, lab_equipment_name: Faker::Device.serial, maker_name: Faker::Device.manufacturer, product_name: Faker::Device.model_name, purchase_year: rand(1990..2020), asset_num: Faker::Device.serial,
+price: rand(1000..500000), remarks: "test", user_id: user.id )
 end
 puts "初期データの作成完了"
