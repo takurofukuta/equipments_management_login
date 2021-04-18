478995e5097fb4d9213ca18bfdbc9f780cd7252d
takurofukuta <mojuraspax@icloud.com>
2021-04-18 14:37:43 +0900

    equipments_helperファイルにsort_orderメソッドを追加

--- app/helpers/equipments_helper.rb
+++ app/helpers/equipments_helper.rb
@@ -1,2 +1,7 @@
 module EquipmentsHelper
+  def sort_order(column, title)
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+
+    link_to title, { sort: column, direction: direction }
+  end
 end
