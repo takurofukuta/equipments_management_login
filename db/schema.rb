72c61da5b1b9869a1636161235b34d51836e48d4
takurofukuta <mojuraspax@icloud.com>
2021-04-18 18:05:01 +0900

    追加・編集・削除・閲覧権限を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_102800) do
+ActiveRecord::Schema.define(version: 2021_04_18_084518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_102800) do
     t.integer "assignment_year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
